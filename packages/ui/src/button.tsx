4751882cfa8b85b3f40844cf31eee439ec6493a2
Dan Lincoln <dan.lincoln@xelix.com>
2025-02-11 10:33:19 +0000

    Untrack styled-system

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,5 +1,5 @@
 import { css } from '@repo/styled-system/css';
 
 export const Button = () => {
-    return <button className={css({ color: 'yellow' })}>Click me</button>
+    return <button className={css({ color: 'pink' })}>Click me</button>
 }
